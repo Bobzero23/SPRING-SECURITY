601f77a07cee2844517120ec6d01b2fa72992578
Bobzero <rajabu.mustafa.00@gmail.com>
2023-09-07 14:54:50 +0300

    implemented authentication provider

---
+++ SpringSecurity/src/main/java/security/AuthProvider.java
@@ -0,0 +1,67 @@
+package security;
+
+import com.SpringSecurity.model.Attempts;
+import com.SpringSecurity.model.User;
+import com.SpringSecurity.repository.AttemptsRepository;
+import com.SpringSecurity.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuthProvider implements AuthenticationProvider {
+
+    private static final int ATTEMPTS_LIMIT = 3;
+    private SecurityUserDetailsService securityUserDetailsService;
+    private PasswordEncoder passwordEncoder;
+    private AttemptsRepository attemptsRepository;
+    private UserRepository userRepository;
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+
+        Optional<Attempts> userAttempts = attemptsRepository.findAttemptsByUsername(username);
+
+        if (userAttempts.isPresent()) {
+            Attempts attempts = userAttempts.get();
+            attempts.setAttempts(0);
+            attemptsRepository.save(attempts);
+        }
+
+        return authentication;
+    }
+
+    private void processFailedAttempts(String username, User user) {
+        Optional<Attempts> userAttempts = attemptsRepository.findAttemptsByUsername(username);
+
+        if(userAttempts.isEmpty()) {
+            Attempts attempts = new Attempts();
+            attempts.setUsername(username);
+            attempts.setAttempts(1);
+            attemptsRepository.save(attempts);
+        }else {
+            Attempts attempts = userAttempts.get();
+            attempts.setAttempts(attempts.getAttempts() + 1);
+            attemptsRepository.save(attempts);
+
+            if(attempts.getAttempts() > ATTEMPTS_LIMIT) {
+                user.setAccountNonLocked(false);
+                userRepository.save(user);
+                throw new LockedException("Too many invalid attempts, Account is locked!!");
+            }
+        }
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+}
--- SpringSecurity/src/main/java/security/SecurityUserDetailsService.java
+++ SpringSecurity/src/main/java/security/SecurityUserDetailsService.java
@@ -1,11 +1,13 @@
-package com.SpringSecurity.services.security;
+package security;
 
 import com.SpringSecurity.model.User;
 import com.SpringSecurity.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SecurityUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
